819728fc5ad3d218e26830aaa3173e485cb63d78
usmanhaider461 <46385291+usmanhaider461@users.noreply.github.com>
2020-11-25 22:58:08 +0500

    SetState has been settled with increment to new Question

--- lib/main.dart
+++ lib/main.dart
@@ -30,14 +30,13 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List <Icon> scoreKeeper = [Icon(
-    Icons.check,
-    color: Colors.green,
-  ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    )];
+  List <Icon> scoreKeeper = [];
+  List<String> question = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -50,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the Question will go',
+                question[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -78,8 +77,8 @@ class _QuizPageState extends State<QuizPage> {
                   scoreKeeper.add(
                     Icon(Icons.check,
                       color: Colors.green,),
-
                   );
+                  questionNumber++;
                 });
 
               },
@@ -104,10 +103,9 @@ class _QuizPageState extends State<QuizPage> {
                     scoreKeeper.add(
                       Icon(Icons.close,
                         color: Colors.red,),
-
                     );
+                    questionNumber++;
                   });
-
                 },
               )
           ),
