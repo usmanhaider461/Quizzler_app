050e203621000551c2d5c0e26624ea128cba8149
usmanhaider461 <46385291+usmanhaider461@users.noreply.github.com>
2020-11-25 20:15:03 +0500

    Setting up Project

---
+++ android/app/src/main/kotlin/com/example/Quizzler_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Quizzler_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
